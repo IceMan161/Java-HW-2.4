cac5f39edfe14aa16fbc265733b3d54f6b49cba1
Artem Sukhanov <IceMan161@yandex.ru>
2021-11-25 15:24:12 +0300

    JavaHW-2.4 corrected

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -20,7 +20,7 @@ class SQRServiceTest {
     void squareCounterInvalidValueWithinTheBoundaries() {
         SQRService sqrService = new SQRService();
 
-        int expected = 4;
+        int expected = 3;
         int actual = sqrService.squareCounter(200,300);
 
         assertEquals(expected,actual);
@@ -40,10 +40,29 @@ class SQRServiceTest {
     void squareCounterBoundaryNumbersBeyond() {
         SQRService sqrService = new SQRService();
 
-        int expected = 3;
+        int expected = 2;
         int actual = sqrService.squareCounter(226,289);
 
         assertEquals(expected,actual);
     }
 
+    @Test
+    void squareCounterGettingOneSquareIntoTheBoundaries() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 1;
+        int actual = sqrService.squareCounter(224,226);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void squareCounterGettingOneSquareIntoTheBoundariesIncludingTheSumOfSquare() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 1;
+        int actual = sqrService.squareCounter(225,225);
+
+        assertEquals(expected,actual);
+    }
 }
